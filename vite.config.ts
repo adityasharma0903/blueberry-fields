720d014bf44937792dc34a47f886af0812cf04a7
adityasharma0903 <adityasharma.5672@gmail.com>
2025-10-02 19:43:16 +0530

    refresh issue resolved

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    // Add this line to fix SPA refresh 404
+    historyApiFallback: true,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
